a077ae2f3aa8c237e8d9187eb9943dff075872b3
noverd <dasfasru@gmail.com>
2024-08-29 21:35:01 +0300

    Solution scan

---
+++ ArabicaCliento/Systems/ArabicaChemicalScanSystem.cs
@@ -0,0 +1,6 @@
+using ArabicaCliento.Systems.Abstract;
+using Content.Shared.Chemistry.Components;
+
+namespace ArabicaCliento.Systems;
+
+public class ArabicaChemicalScanSystem: LocalPlayerAddCompSystem<SolutionScannerComponent>;
