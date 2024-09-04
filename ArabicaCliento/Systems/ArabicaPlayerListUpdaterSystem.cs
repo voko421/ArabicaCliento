2b2e80d1823770c36e7763422d0d7b581d195548
noverd <dasfasru@gmail.com>
2024-09-04 09:28:43 +0300

    autoupdate

--- ArabicaCliento/Systems/ArabicaPlayerListUpdaterSystem.cs
+++ ArabicaCliento/Systems/ArabicaPlayerListUpdaterSystem.cs
@@ -1,24 +1,40 @@
 using Content.Client.Chat.Managers;
 using Robust.Client.Player;
+using Robust.Shared.Network;
+using Robust.Shared.Network.Messages;
+using Robust.Shared.Timing;
 
 namespace ArabicaCliento.Systems;
 
 public sealed class ArabicaPlayerListUpdaterSystem: EntitySystem
 {
-    [Dependency] private readonly IChatManager _chat = default!;
+    private static readonly TimeSpan UpdateTime = TimeSpan.FromSeconds(4);
+    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IPlayerManager _player = default!;
+    [Dependency] private readonly INetManager _net = default!;
+
+    private TimeSpan? _nextUpdate = null;
 
     public override void Initialize()
     {
+        _nextUpdate = _timing.CurTime + UpdateTime;
         _player.PlayerListUpdated += OnPlayerListUpdated;
+    }
+
+    public override void Update(float frameTime)
+    {
+        if (_timing.CurTime < _nextUpdate)
+            _net.ClientSendMessage(new MsgPlayerListReq());
     }
 
     public override void Shutdown()
     {
+        _nextUpdate = null;
         _player.PlayerListUpdated -= OnPlayerListUpdated;
     }
 
     private void OnPlayerListUpdated()
     {
+        
     }
 }
