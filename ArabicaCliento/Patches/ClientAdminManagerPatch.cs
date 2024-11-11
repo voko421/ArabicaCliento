b1cad1f3b685b0db0afd83f051707ba08f99fe09
noverd <you@example.com>
2024-11-11 21:37:33 +0300

    Admin menu fix.

--- ArabicaCliento/Patches/ClientAdminManagerPatch.cs
+++ ArabicaCliento/Patches/ClientAdminManagerPatch.cs
@@ -1,16 +1,26 @@
+using System.Reflection;
 using Content.Client.Administration.Managers;
 using HarmonyLib;
 
 namespace ArabicaCliento.Patches;
 
 [HarmonyPatch(typeof(ClientAdminManager))]
-[HarmonyPatch(nameof(ClientAdminManager.CanAdminMenu))]
-[HarmonyPatch(nameof(ClientAdminManager.CanAdminPlace))]
-[HarmonyPatch(nameof(ClientAdminManager.CanScript))]
-[HarmonyPatch(nameof(ClientAdminManager.CanCommand))]
-[HarmonyPatch(nameof(ClientAdminManager.IsActive))]
 internal static class ClientAdminManagerPatch
 {
-    [HarmonyPostfix]
-    private static void Postfix(ref bool __result) => __result = true;
+    [HarmonyTargetMethods]
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(ClientAdminManager), nameof(ClientAdminManager.CanAdminMenu));
+        yield return AccessTools.Method(typeof(ClientAdminManager), nameof(ClientAdminManager.CanAdminPlace));
+        yield return AccessTools.Method(typeof(ClientAdminManager), nameof(ClientAdminManager.CanScript));
+        yield return AccessTools.Method(typeof(ClientAdminManager), nameof(ClientAdminManager.CanCommand));
+        yield return AccessTools.Method(typeof(ClientAdminManager), nameof(ClientAdminManager.IsActive));
+    }
+
+    [HarmonyPrefix]
+    private static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
 }
--- ArabicaCliento/Patches/ClientConGroupControllerPatch.cs
+++ ArabicaCliento/Patches/ClientConGroupControllerPatch.cs
@@ -1,16 +1,28 @@
+using System.Reflection;
 using HarmonyLib;
 using Robust.Client.Console;
 
 namespace ArabicaCliento.Patches;
 
 [HarmonyPatch(typeof(ClientConGroupController))]
-[HarmonyPatch(nameof(ClientConGroupController.CanCommand))]
-[HarmonyPatch(nameof(ClientConGroupController.CanScript))]
-[HarmonyPatch(nameof(ClientConGroupController.CanAdminMenu))]
-[HarmonyPatch(nameof(ClientConGroupController.CanAdminPlace))]
-[HarmonyPatch(nameof(ClientConGroupController.CanViewVar))]
 internal static class ClientConGroupControllerPatch
 {
-    [HarmonyPostfix]
-    private static void Postfix(ref bool __result) => __result = true;
+    [HarmonyTargetMethods]
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(ClientConGroupController),
+            nameof(ClientConGroupController.CanAdminMenu));
+        yield return AccessTools.Method(typeof(ClientConGroupController),
+            nameof(ClientConGroupController.CanAdminPlace));
+        yield return AccessTools.Method(typeof(ClientConGroupController), nameof(ClientConGroupController.CanScript));
+        yield return AccessTools.Method(typeof(ClientConGroupController), nameof(ClientConGroupController.CanCommand));
+        yield return AccessTools.Method(typeof(ClientConGroupController), nameof(ClientConGroupController.CanViewVar));
+    }
+
+    [HarmonyPrefix]
+    private static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
 }
