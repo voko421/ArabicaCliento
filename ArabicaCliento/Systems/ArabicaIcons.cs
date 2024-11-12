ab5ccad8afe7caf9c862388a236a28dc0ed696f7
noverd <you@example.com>
2024-11-12 22:36:01 +0300

    Icons update

--- ArabicaCliento/Systems/ArabicaIcons.cs
+++ ArabicaCliento/Systems/ArabicaIcons.cs
@@ -6,3 +6,5 @@ namespace ArabicaCliento.Systems;
 public class ArabicaJobIconsSystem : LocalPlayerAddCompSystem<ShowJobIconsComponent>;
 public class ArabicaCriminalRecordIcons : LocalPlayerAddCompSystem<ShowCriminalRecordIconsComponent>;
 public class ArabicaMindShieldIcons : LocalPlayerAddCompSystem<ShowMindShieldIconsComponent>;
+
+public class ArabicaSyndicateIcons : LocalPlayerAddCompSystem<ShowSyndicateIconsComponent>;
