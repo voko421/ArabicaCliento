6996aa030ef8074f4e242198d2f3d134270d54da
noverd <71782778+noverd@users.noreply.github.com>
2025-07-15 11:19:40 +0300

    fix: ci/cd for net9.0

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 dotnet build
-yes | cp ArabicaCliento/bin/Debug/net8.0/ArabicaCliento.dll ~/marsey_loader/Marsey/Mods/ArabicaCliento.dll
+yes | cp ArabicaCliento/bin/Debug/net9.0/ArabicaCliento.dll ~/marsey_loader/Marsey/Mods/ArabicaCliento.dll
